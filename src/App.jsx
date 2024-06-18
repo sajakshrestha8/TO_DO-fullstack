4e683c4bdda48a0eabfcd0960f8a39ef89898519
sajakshrestha8 <shresthasajak123@gmail.com>
2024-06-18 09:25:32 +0545

    completed design

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Components/Button";
 import Input from "./Components/Inputfield";
 import "./CSS/index.css";
 
 export default function App() {
+  useEffect(() => {
+    fetch("http://localhost:8001/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setTask(data);
+      });
+  }, []);
+
   const [typing, setTyping] = useState("");
-  const [task, setTask] = useState([
-    {
-      task: "Go to Gym",
-    },
-    {
-      task: "Go to sleep",
-    },
-  ]);
+  const [task, setTask] = useState([]);
 
   const enteredTask = (e) => {
     const typedData = e.target.value;
@@ -28,44 +32,63 @@ export default function App() {
       },
     ];
     setTask(addedData);
+
+    fetch("http://localhost:8001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addedData),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
     <>
-      <div className="add-input">
-        <div className="input-wrapper">
-          <Input placeholder="Enter your task here" typing={enteredTask} />
+      <div className="to-do-wrapper">
+        <div className="heading">
+          <h1>TO DO LIST</h1>
         </div>
-        <div>
-          <Button btn="ADD" click={addTask} />
-        </div>
-      </div>
-      {task?.map((value, index) => {
-        return (
-          <div className="task-wrapper" key={index}>
-            <div className="checkbox-wrapper">
-              <input type="checkbox" className="checkbox" />
-            </div>
-            <div>
-              <label className="task">{value.task}</label>
-            </div>
-            <div>
-              <Button btn="UPDATE" />
-            </div>
-            <div>
-              <Button
-                btn="DELETE"
-                click={() => {
-                  const updatedArr = task?.filter((value, idx) => {
-                    return value.index !== idx;
-                  });
-                  setTask(updatedArr);
-                }}
-              />
-            </div>
+        <div className="add-input">
+          <div className="input-wrapper">
+            <Input placeholder="Enter your task here" typing={enteredTask} />
           </div>
-        );
-      })}
+          <div>
+            <Button btn="ADD" click={addTask} />
+          </div>
+        </div>
+        {task?.map((value, index) => {
+          return (
+            <div className="task-wrapper" key={index}>
+              <div className="checkbox-wrapper">
+                <input type="checkbox" className="checkbox" />
+              </div>
+              <div>
+                <label className="task">{value.task}</label>
+              </div>
+              <div>
+                <Button btn="UPDATE" />
+              </div>
+              <div>
+                <Button
+                  btn="DELETE"
+                  click={() => {
+                    const updatedArr = task?.filter((value, idx) => {
+                      return value.index !== idx;
+                    });
+                    setTask(updatedArr);
+                  }}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
--- todo backend/index.js
+++ todo backend/index.js
@@ -1,31 +1,32 @@
+/* eslint-disable no-undef */
 const express = require("express");
 const app = express();
-let toDos=[{
-  task:"Go for a walk",
-  isCompleted: false,
-},
-{
-task:"Read a book",
-isCompleted: false
-}
-]
+const cors = require("cors");
 
+let toDos = [
+  {
+    task: "Go for a walk",
+    isCompleted: false,
+  },
+  {
+    task: "Read a book",
+    isCompleted: false,
+  },
+];
+
+app.use(cors());
 app.use(express.json());
 
-
- app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.send(toDos);
- });
+});
 
- app.post('/',(req,res)=>{
-  const payload= req.body;
+app.post("/", (req, res) => {
+  const payload = req.body;
 
   toDos.push(payload);
-  res.send({message: "New user has been created",
-    data: payload,
-  });
-
- })
+  res.send({ message: "New user has been created", data: payload });
+});
 
 app.listen(8001, () => {
   console.log("server running");
